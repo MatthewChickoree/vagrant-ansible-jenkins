5ebdd276a8136e3e92252c085deb84f3fe3c4896
Layton Whiteley <layton.whiteley@gmail.com>
2014-09-29 16:09:47 -0400

    Generated by grunt gh-pages

--- lunr-index.js
+++ lunr-index.js
@@ -12,7 +12,7 @@ documentTitles["home.html#home"] = "Home";
 index.add({
     url: "home.html#home",
     title: "Home",
-    body: "# Home  Welcome to the vagrant-ansible-jenkins documentation!  To get started view the Introduction page,  otherwise, select the specific topic that you require.  Jump right in!   ![github](http://ciembor.github.io/4bit/images/github.png)  [Repository](https://github.com/medullan/vagrant-ansible-jenkins)   ![wiki](http://www.altera.com/common/template/08/icon-wiki.gif)  [Edit On Guthub](https://github.com/medullan/vagrant-ansible-jenkins.wiki.git)   "
+    body: "# Home  Welcome to the vagrant-ansible-jenkins documentation!  To get started view the Introduction page,  otherwise, select the specific topic that you require.  Jump right in!   ![github](http://ciembor.github.io/4bit/images/github.png)  [Repository](https://github.com/medullan/vagrant-ansible-jenkins)   ![wiki](http://www.altera.com/common/template/08/icon-wiki.gif)  [Edit On Guthub](https://github.com/medullan/vagrant-ansible-jenkins/wiki)   "
 });
 
 documentTitles["home.html#useful-links"] = "Useful Links:";
